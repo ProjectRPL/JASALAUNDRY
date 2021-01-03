fb6b9bca0f5a0db8717bdd627f1b852e0cf3e578
melyndaputri <75265741+melyndaputri@users.noreply.github.com>
2021-01-03 23:07:07 +0700

    Update Jasa_Laundry.java

--- Jasa_Laundry.java
+++ Jasa_Laundry.java
@@ -9,6 +9,7 @@
 // 
 // 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 import java.util.*;
 public class Jasa_Laundry{
     public static void main(String[] args){
@@ -20,10 +21,19 @@ public class Jasa_Laundry{
         System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         Scanner input = new Scanner(System.in);
         String aaa = "yes";
+        String nama ="";
+        String alamat ="";
+        DecimalFormat digit = new DecimalFormat("0.00");
+        
 
         System.out.println("MASUKKAN DATA DIRI ANDA");
-        System.out.print("NAMA : ");
-        String nama = input.nextLine();
+        do {
+	        System.out.print("NAMA : ");
+	        nama = input.nextLine();
+	        if (!nama.matches("[a-zA-Z\\s]+")) {
+	              System.out.println("Masukan Yang Anda beri, Salah\n");
+	            }
+	    } while (!nama . matches ("[a-zA-Z\\s]+"));
         do{
             System.out.print("KOTA ASAL : ");
             String kota_Asal = input.nextLine();
@@ -36,11 +46,15 @@ public class Jasa_Laundry{
                      || kota_Asal.equalsIgnoreCase("BEKASI SELATAN") || kota_Asal.equalsIgnoreCase("BEKASI TIMUR")
                       || kota_Asal.equalsIgnoreCase("BEKASI") || kota_Asal.equalsIgnoreCase("JAKARTA")){
                 gratisOngkir = "---- SELAMAT ANDA MENDAPATKAN GRATIS ONGKIR ----";
-        
-                System.out.print("ALAMAT TEMPAT TINGGAL : ");
-                String alamat = input.nextLine();
+                do{
+	                System.out.print("ALAMAT TEMPAT TINGGAL : ");
+	                alamat = input.nextLine();
+	                if (!alamat.matches("[a-zA-Z\\s]+")) {
+		              System.out.println("Masukan Yang Anda beri, Salah\n");
+		            }
+	            } while (!alamat . matches ("[a-zA-Z\\s]+"));
                 String pilihan_jenis_Paket="";
-                int harga_paket=0;
+                double harga_paket=0;
                 String bbb ="";
                 do{ 
                     System.out.println("JENIS PAKET ");
@@ -71,7 +85,7 @@ public class Jasa_Laundry{
                 int a = 1;
                 int b = 0;
                 int harga_perpakaian=0;
-                int hagra_jumlah_pakaian = 0;
+                double hagra_jumlah_pakaian = 0;
                 String bb = "";
                 String pilihan_jenis_Pakaian="";
                 do {
@@ -140,30 +154,47 @@ public class Jasa_Laundry{
                                 System.out.println("------------SILAKAN PILIH KEMBALI------------");
                                 bb="yes";
                             }
-                        }while (bb.equalsIgnoreCase("yes"));
+                    }while (bb.equalsIgnoreCase("yes"));
                     
                         for (int i = b; i < a ; i++) {
                           macam_pakaian[i] = pilihan_jenis_Pakaian;
                           hagra_jumlah_pakaian+=harga_perpakaian;
                         }
                         System.out.println("");
-                        System.out.print("Pilih jenis pakaian kembali (y/n)?");
-                        pilih_kembali = input.nextLine();
-                        b++;
-                        a++;
+                        
+	                    do{
+	                    	System.out.print("Pilih jenis pakaian kembali (y/n)?");
+	                        pilih_kembali = input.nextLine();
+		                    if (pilih_kembali.equals("y")) {
+		                    	b++;
+                                a++;
+	                
+	                        }else if(pilih_kembali.equals("n")){
+                                b++;
+                                a++;
+	                        	break;
+	                        }else{
+	                        	System.out.println("Masukan Yang Anda beri, Salah\n");
+	                        }
+	                    }while (!pilih_kembali.equals("n") && !pilih_kembali.equals("y"));
                 }
                 while (pilih_kembali.equalsIgnoreCase(yes)); 
                 System.out.println("");
-                
-                System.out.print("MASUKKAN BERAT PAKAIAN DALAM SATUAN (KG) : ");
-                String berat = input.nextLine();
+                double berat ;
+                do {
+                    System.out.print("MASUKKAN BERAT PAKAIAN DALAM SATUAN (KG) : ");
+                    berat = input.nextDouble();
+                    input.nextLine();
+                    if (berat > 200.0 || berat < 0.0) {
+                          System.out.println("Berat tidak boleh lebih dari 200KG\n");
+                        }
+                } while (berat > 200.0 || berat < 0.0);
+
                 String dapat_diskon = "";
-                int diskon = 0;
-                int harga_kiloan = 0;
-                for(int i = 0; i < Integer.parseInt(berat) ; i++){
-                    harga_kiloan+=harga_paket;
-                }
-                if(Integer.parseInt(berat) > 5){
+                double diskon = 0;
+                double harga_kiloan = 0;
+                harga_kiloan = harga_paket * berat;
+                if((berat) > 5){
                     diskon = (hagra_jumlah_pakaian * 10)/100;
                     diskon = hagra_jumlah_pakaian - diskon + harga_kiloan;
                     dapat_diskon = "--------- SELAMAT ANDA MENDAPATKAN DISKON 10% --------";
@@ -183,40 +214,63 @@ public class Jasa_Laundry{
                 for (int i = 0; i < b ; i++) {
                     System.out.println((i+1)+". "+macam_pakaian[i]);
                 }
-                System.out.println("BERAT PAKAIAN DALAM SATUAN (KG) : "+berat);
+                System.out.println("BERAT PAKAIAN DALAM SATUAN (KG) : "+berat+" KG");
                 System.out.println("");
                 System.out.println(dapat_diskon);
                 System.out.println(gratisOngkir);
                 
                 System.out.println("");
-                //System.out.println("HARGA PAKET PAKAIAN : " + harga_paket +" * "+ Integer.parseInt(berat) + " = " + (harga_paket*Integer.parseInt(berat)));
-                //System.out.println("HARGA JENIS PAKAIAN : " + harga_perpakaian);
-                System.out.println("TOTAL HARGA : Rp." + diskon);
+                System.out.println("TOTAL HARGA : Rp." + digit.format(diskon));
                 System.out.println("\n------------------------------------------------");
-                System.out.print("Lanjut ke pembayaran (y/n)?");
-                String lanjut_Pembayaran = input.nextLine();
+                String lanjut_Pembayaran = "";
 
-                if(lanjut_Pembayaran.equalsIgnoreCase("y")){
-                    System.out.println("Pilih Metode Pembayaran:");
-                    System.out.println("1. Tunai");
-                    System.out.println("2. Debit");
-                    System.out.print("PILIH METODE PEMBAYARAN : ");
-                    String jenis_Pembayaran = input.nextLine();
-                    switch (jenis_Pembayaran) {
-                        case "1":
-                            System.out.println("SILAHKAN MELAKUKAN PEMBAYARAN MELALUI KURIR");
-                            System.out.println("-----SILAHKAN MENUNGGU BARANG DIANTAR-----");
-                             aaa = "no";
+
+                do{
+                	System.out.print("Lanjut ke pembayaran (y/n)?");
+                    lanjut_Pembayaran = input.nextLine();
+	                if (lanjut_Pembayaran.equals("y")) {
+	                	System.out.println("Pilih Metode Pembayaran:");
+	                    System.out.println("1. Tunai");
+	                    System.out.println("2. Debit");
+	                    String jenis_Pembayaran = "";
+
+	                    do{
+			                System.out.print("PILIH METODE PEMBAYARAN : ");
+	                        jenis_Pembayaran = input.nextLine();
+			                if (jenis_Pembayaran.matches("[a-zA-Z\\s]+")) {
+				              System.out.println("Masukan Yang Anda beri, Salah\n");
+				            }
+			            } while (jenis_Pembayaran.matches ("[a-zA-Z\\s]+"));
+
+
+
+	                    switch (jenis_Pembayaran) {
+	                        case "1":
+	                            System.out.println("SILAHKAN MELAKUKAN PEMBAYARAN MELALUI KURIR");
+	                            System.out.println("-----SILAHKAN MENUNGGU BARANG DIANTAR-----");
+	                             aaa = "no";
+	                            break;
+	                    
+	                        case "2":
+	                            System.out.println("PEMBAYARAN SEDANG DI PROSES---------------");
+	                            System.out.println("-----SILAHKAN MENUNGGU BARANG DIANTAR-----");
+	                            aaa = "no";
+	                            break;
+                            default:
+                            System.out.println("Masukan Yang Anda beri, Salah\n");
                             break;
-                    
-                        case "2":
-                            System.out.println("PEMBAYARAN SEDANG DI PROSES---------------");
-                            System.out.println("-----SILAHKAN MENUNGGU BARANG DIANTAR-----");
-                            aaa = "no";
-                            break;
-                            
-                    }
-                }
+
+	                            
+	                    }
+		               
+		            }else if(lanjut_Pembayaran.equals("n")){
+		            	break;
+		            }else{
+		            	System.out.println("Masukan Yang Anda beri, Salah\n");
+                       
+	                }
+	            }while (!lanjut_Pembayaran.equals("y") && !lanjut_Pembayaran.equals("n"));
+
                 aaa = "no";
             }else{
                 System.out.println("\n----------------------------------------------------");
